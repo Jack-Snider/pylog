ce89bc1bfd0861e0306563ae1c1a282a4d37ddd3
= <choiman2021@gmail.com>
2023-04-08 23:22:20 +0900

    이미지 파일 업로드하기

--- config/urls.py
+++ config/urls.py
@@ -18,9 +18,21 @@ from django.contrib import admin
 from django.urls import path
 from config.views import index # index import
 from blog.views import post_list
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path( "", index ), # 경로가 없을 때 index View 연결
     path( "posts/", post_list ),
 ]
+
+urlpatterns += static(
+
+    # URL의 접두어가 MEDIA_URL일 때는 정적파일을 돌려준다.
+    prefix = settings.MEDIA_URL,
+
+    # 돌려줄 디렉터리는 MEDIA_ROOT를 기준으로 한다.
+    document_root = settings.MEDIA_ROOT,
+
+)
