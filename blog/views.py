397e0fb2d50bce49278e70d8d7496fb53f83e766
= <choiman2021@gmail.com>
2023-04-08 13:40:10 +0900

    post_list로 게시물 목록 만들기

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from blog.models import Post # Post 모델을 사용하기 위해 import
 # Create your views here.
+
+def post_list( request ):
+
+    posts = Post.objects.all() # 모든 Post 객체를 가진 QuerySet
+
+    # 템플릿에 전달한 dict
+    context = {
+        "posts" : posts,
+    }
+
+    # 3번째 인수로 템플릿에 데이터를 전달
+    return render(request, "post_list.html", context )
--- config/urls.py
+++ config/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from config.views import index # index import
+from blog.views import post_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path( "", index ), # 경로가 없을 때 index View 연결
+    path( "posts/", post_list ),
 ]
